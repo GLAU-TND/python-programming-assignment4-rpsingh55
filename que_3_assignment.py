e346feeb5a1501501d64a276df2803a1f9579bc1
rpsingh55 <60340745+rpsingh55@users.noreply.github.com>
2020-04-01 14:42:35 +0530

    Update que_3_assignment.py

--- que_3_assignment.py
+++ que_3_assignment.py
@@ -1,9 +1,4 @@
 a=eval(input())
 s=input()
-d=[]
-for i in a:
-    for j in range (len(i)):
-        if i[:j]==s:
-            d.append(i)
-            berak
+d=[i for i in a if i[:len(s)]==s]
 print(d)
